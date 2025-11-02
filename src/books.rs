39ec593f69e54cd9fc4759ed3acff5af18665b25
Mathieu Schroeter <mathieu@schroetersa.ch>
2025-11-02 23:41:04 +0100

    Add initial books struct to load all books

---
+++ src/books.rs
@@ -0,0 +1,29 @@
+use crate::book::Book;
+use anyhow::Result;
+use std::{fs, path::Path};
+
+pub struct Books {
+    books: Vec<Book>,
+}
+
+impl Books {
+    pub fn from_dir(path: &Path) -> Result<Self> {
+        let mut books = Vec::new();
+
+        for entry in fs::read_dir(path)? {
+            let entry = entry?;
+            let path = entry.path();
+            if !path.is_dir() {
+                continue;
+            }
+
+            let story_path = path.join("story.json");
+            match Book::from_file(&story_path) {
+                Ok(book) => books.push(book),
+                Err(e) => eprintln!("Cannot load the book {:?}: {}", story_path, e),
+            }
+        }
+
+        Ok(Books { books })
+    }
+}
