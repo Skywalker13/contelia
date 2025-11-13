58459d35b1ca7e5e633f48b3ffc3e675f2d978eb
Mathieu Schroeter <mathieu@schroetersa.ch>
2025-11-13 23:47:31 +0100

    Reduce and center image without cropping (at least in height)

--- src/renderer.rs
+++ src/renderer.rs
@@ -17,24 +17,27 @@ impl Renderer {
         let width = self.fb.var_screen_info.xres;
         let height = self.fb.var_screen_info.yres;
         let line_length = self.fb.fix_screen_info.line_length;
-        let img = image::open(image)?.resize_to_fill(
-            width,
-            height,
-            image::imageops::FilterType::CatmullRom,
-        );
+        let img =
+            image::open(image)?.resize(width, height, image::imageops::FilterType::CatmullRom);
 
-        let (w, h) = img.dimensions();
-        for y in 0..h.min(height) {
-            for x in 0..w.min(width) {
-                let pixel = img.get_pixel(x, y);
+        let (_w, h) = img.dimensions();
+        let h_offset = if height > h { (height - h) / 2 } else { 0 };
+        for y in 0..height {
+            for x in 0..width {
                 let offset = ((y * line_length / 2) + x) as usize;
 
-                let rgb565 = ((pixel[0] as u16 >> 3) << 11)
-                    | ((pixel[1] as u16 >> 2) << 5)
-                    | (pixel[2] as u16 >> 3);
+                if y >= h_offset && y - h_offset < h {
+                    let pixel = img.get_pixel(x, y - h_offset);
+                    let rgb565 = ((pixel[0] as u16 >> 3) << 11)
+                        | ((pixel[1] as u16 >> 2) << 5)
+                        | (pixel[2] as u16 >> 3);
 
-                self.fb.frame[offset * 2] = (rgb565 & 0xFF) as u8;
-                self.fb.frame[offset * 2 + 1] = (rgb565 >> 8) as u8;
+                    self.fb.frame[offset * 2] = (rgb565 & 0xFF) as u8;
+                    self.fb.frame[offset * 2 + 1] = (rgb565 >> 8) as u8;
+                } else {
+                    self.fb.frame[offset * 2] = 0 as u8;
+                    self.fb.frame[offset * 2 + 1] = 0 as u8;
+                };
             }
         }
 
