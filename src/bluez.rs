e509b4844fc96490f81dd2d6ee6fc63bbdf4f7d3
Mathieu Schroeter <mathieu@schroetersa.ch>
2026-03-23 23:04:59 +0100

    Restore some comments

--- src/bluez.rs
+++ src/bluez.rs
@@ -218,9 +218,9 @@ impl Bluez {
         props: &HashMap<String, OwnedValue>,
     ) -> Option<Device> {
         const AUDIO_UUIDS: &[&str] = &[
-            "0000110b-0000-1000-8000-00805f9b34fb",
-            "00001108-0000-1000-8000-00805f9b34fb",
-            "0000111e-0000-1000-8000-00805f9b34fb",
+            "0000110b-0000-1000-8000-00805f9b34fb", /* A2DP Sink */
+            "00001108-0000-1000-8000-00805f9b34fb", /* HSP       */
+            "0000111e-0000-1000-8000-00805f9b34fb", /* HFP       */
         ];
 
         let name = props
