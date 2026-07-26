62279bf736c10530acd183aebf0f1e2212544e4e
Mathieu Schroeter <mathieu@schroetersa.ch>
2026-07-26 17:55:40 +0200

    Add minimal support for alpha channel

--- src/screen.rs
+++ src/screen.rs
@@ -82,6 +82,9 @@ impl Screen {
 
                 if y >= h_offset && y - h_offset < h {
                     let pixel = img.get_pixel(x, y - h_offset);
+                    if pixel[3] == 0x00 {
+                        continue;
+                    }
                     let rgb565 = ((pixel[0] as u16 >> 3) << 11)
                         | ((pixel[1] as u16 >> 2) << 5)
                         | (pixel[2] as u16 >> 3);
