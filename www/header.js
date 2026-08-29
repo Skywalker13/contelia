97d9460bd2de8ed00b872bac00e3fef7fd316ab1
Mathieu Schroeter <mathieu@schroetersa.ch>
2026-08-29 23:06:52 +0200

    Ensure to redirect only the first time

--- www/header.js
+++ www/header.js
@@ -1,4 +1,7 @@
-if (/android/i.test(navigator.userAgent)) {
-  window.location.href =
-    "intent://${window.location.host}${window.location.pathname}#Intent;scheme=http;end";
+if (
+  /android/i.test(navigator.userAgent) &&
+  !sessionStorage.getItem("redirected")
+) {
+  sessionStorage.setItem("redirected", "true");
+  window.location.href = `intent://${window.location.host}${window.location.pathname}#Intent;scheme=http;end`;
 }
